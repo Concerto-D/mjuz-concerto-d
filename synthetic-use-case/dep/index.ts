b800d970c15442774b6098fc21f21b3061214144
aomond <antoine.omond@imt-atlantique.fr>
2022-11-05 09:37:31 +0100

    Generalize to n deps

--- synthetic-use-case/dep/index.ts
+++ synthetic-use-case/dep/index.ts
@@ -10,6 +10,9 @@ import {
 import { Offer, RemoteConnection } from '@mjuz/core/resources';
 import { Behavior } from '@funkia/hareactive';
 import {DepInstallResource} from "../depInstall";
+import * as fs from "fs";
+import * as YAML from "yaml";
+
 
 const config_file_path = process.argv[4];
 const timestamp_log_file = process.argv[5];
@@ -29,8 +32,14 @@ console.log(
 )
 console.log("------------")
 
+const inventory = YAML.parse(fs.readFileSync("/home/anomond/concerto-d-projects/mjuz-concerto-d/inventory.yaml", "utf-8"))
+console.log("inventory:")
+console.log(inventory)
+console.log("----------")
+
 const program = async () => {
-	const contentManager = new RemoteConnection(`dep${depNum}`, { port: 19952 });
+	const serverHost = inventory["server"].split(":")[0]
+	const contentManager = new RemoteConnection(`dep${depNum}`, { port: 19952, host: serverHost});
 	const depInstallRessource = new DepInstallResource(`dep${depNum}Install`, {name: `dep${depNum}Install`, time: 1.1});
 	new Offer(contentManager, `dep${depNum}Install`, depInstallRessource)
 	
--- synthetic-use-case/server/index.ts
+++ synthetic-use-case/server/index.ts
@@ -11,12 +11,14 @@ import { Wish, RemoteConnection } from '@mjuz/core/resources';
 import { Behavior } from '@funkia/hareactive';
 import {ServerInstallResource} from "../serverInstall";
 import {DepInstallResource} from "../depInstall";
+import * as fs from "fs";
+import * as YAML from "yaml";
 
 const config_file_path = process.argv[4];
 const timestamp_log_file = process.argv[5];
 const g5k_execution_params_dir = process.argv[6];
 const reconfiguration_name = process.argv[7];
-const nb_concerto_nodes = process.argv[8];
+const nb_concerto_nodes = Number.parseInt(process.argv[8]);
 
 console.log("script parameters:")
 console.log(
@@ -28,17 +30,37 @@ console.log(
 )
 console.log("------------")
 
+const inventory = YAML.parse(fs.readFileSync("/home/anomond/concerto-d-projects/mjuz-concerto-d/inventory.yaml", "utf-8"))
+console.log("inventory:")
+console.log(inventory)
+console.log("----------")
+
+
 const program = async () => {
-	const contentManager0 = new RemoteConnection('dep0', { port: 19954 });
-	const contentManager1 = new RemoteConnection('dep1', { port: 19956 });
-	const dep0InstallWish = new Wish<DepInstallResource>(contentManager0, "dep0Install")
-	const dep1InstallWish = new Wish<DepInstallResource>(contentManager1, "dep1Install")
+	// const contentManager0 = new RemoteConnection('dep0', { port: 19954 });
+	// const contentManager1 = new RemoteConnection('dep1', { port: 19956 });
+	// const dep0InstallWish = new Wish<DepInstallResource>(contentManager0, "dep0Install");
+	// const dep1InstallWish = new Wish<DepInstallResource>(contentManager1, "dep1Install")
+	const remoteConns = [];
+	const wishes = [];
+	const depsOffers = [];
+	let depNum: number;
+	for (depNum = 0; depNum < nb_concerto_nodes; depNum++) {
+		const depName = `dep${depNum}`;
+		const depHost = inventory[depName].split(":")[0];
+		let remoteConn = new RemoteConnection(depName, { port: 19954 + 2*depNum, host: depHost});
+		let depWish = new Wish<DepInstallResource>(remoteConn, `dep${depNum}Install`);
+		remoteConns.push(remoteConn);
+		wishes.push(depWish);
+		depsOffers.push(depWish.offer);
+	}
+	
 	const serverInstallRessource = new ServerInstallResource(
 		"serverInstall",
 		{
 			name: "serverInstall", 
 			time: 5.3,
-			depsOffers: [dep0InstallWish.offer, dep1InstallWish.offer]
+			depsOffers: depsOffers
 		});
 	
 	return {
