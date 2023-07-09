65b104e919fb926a212a02c5da21a6e2b3fd7544
aomond <antoine.omond@imt-atlantique.fr>
2023-07-09 14:29:57 +0200

    Added chained-dep-2-components to test chained dep, server, chained-dep

---
+++ reset.sh
@@ -0,0 +1,4 @@
+#/usr/bin/bash
+
+./reset_pulumi_ts-nodes.sh
+./reset_pulumi_dir.sh
---
+++ synthetic-use-case/chained-dep-2-components/index.ts
@@ -0,0 +1,92 @@
+import {
+	emptyProgram,
+	getStack,
+	nextAction,
+	operations,
+	runDeployment,
+	sigint,
+	sigquit,
+} from '@mjuz/core';
+import { Offer, RemoteConnection } from '@mjuz/core/resources';
+import { Behavior } from '@funkia/hareactive';
+import {DepInstallResource} from "../depInstall";
+import { initializeReconf } from "../metricAnalysis";
+
+
+const [
+	config_file_path,
+	timestamp_log_file,
+	current_execution_dir,
+	reconfiguration_name,
+	nb_concerto_nodes,
+	depNum,
+	inventory,
+	installTime,
+	runningTime,
+	updateTime,
+	logger
+] = initializeReconf("dep")
+
+logger.info("script parameters:")
+logger.info(config_file_path)
+logger.info(timestamp_log_file)
+logger.info(current_execution_dir)
+logger.info(reconfiguration_name)
+logger.info(`${nb_concerto_nodes}`)
+logger.info(`${depNum}`)
+logger.info("------------")
+
+const program = async () => {
+	const serverHost = inventory["server"].split(":")[0]
+	const contentManager = new RemoteConnection(`dep${depNum}`, { port: 19952, host: serverHost});
+	const deployTime = reconfiguration_name === "deploy" ? runningTime : updateTime + runningTime
+	logger.info(`dep${depNum} installTime: ${installTime}`);
+	
+	// const deployTime = 7;
+	//  For update: delete and replace resource
+	const depInstallRessource = new DepInstallResource(
+		`dep${depNum}Install`, 
+		{reconfState: "install", time: installTime}
+	);
+
+	new Offer(contentManager, `dep${depNum}install`, depInstallRessource)
+	logger.info(`dep${depNum} deployTime: ${deployTime}`);
+	const depRunningRessource = new DepInstallResource(
+		`dep${depNum}Running${reconfiguration_name}`, 
+		{reconfState: reconfiguration_name, time: deployTime},
+		{dependsOn: depInstallRessource}
+	);
+	
+	// For the update of the offer, need to also delete and replace the offer because the dep resource changed
+	// TODO: check if this is automatically handled by Mjuz
+	// NOTE: Very ad-hoc solution to prevent Mjuz from blocking because the Offer has to be deleted
+	// and so it has to withdraw from Wish (which is deleted in the server side)
+	// if(reconfiguration_name === 'deploy')
+	new Offer(contentManager, `dep${depNum}${reconfiguration_name}`, depRunningRessource)
+	// if(reconfiguration_name === 'update')
+	// new Offer(contentManager, `dep${depNum}update`, depRunningRessource)
+
+	// Don't let any "dangling" resources (dangling means not exported) because of this issue:
+	// https://github.com/pulumi/pulumi/issues/6998
+	// That's why depInstallRessource and depRunningRessource have to be exported
+	return {
+		depInstallId: depInstallRessource.id,
+		depRunningRessourceId: depRunningRessource.id
+	}
+};
+
+const initStack = getStack(
+	{
+		program: emptyProgram,
+		projectName: `Dep${depNum}DeployAndUpdate2Comps`,
+		stackName: `dep${depNum}2Comps`,
+	},
+	{ workDir: '.' }
+);
+
+runDeployment(
+	initStack,
+	operations(Behavior.of(program)),
+	(offerUpdates) => nextAction(offerUpdates, sigquit(), sigint()),
+	{ deploymentName: `dep${depNum}`, resourcesPort: 19953 + 2*Number.parseInt(depNum), deploymentPort: 19954 + 2*Number.parseInt(depNum)}
+);
